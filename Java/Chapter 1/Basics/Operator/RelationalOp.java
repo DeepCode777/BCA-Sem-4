d5d2ca30d9002bbe7a83c058c1540ec8f6df2262
Code With Deep <deeppoptani777@gmail.com>
2025-12-16 12:40:12 +0530

    Create RelationalOp.java

---
+++ Java/Chapter 1/Basics/Operator/RelationalOp.java
@@ -0,0 +1,13 @@
+public class RelationalOp
+{
+    public static void main(String[] args) {
+        int a = 3,b = 2;
+        
+        System.out.println("Greater Then > : " +(a > b));
+        System.out.println("Less Then < : " +(a < b));
+        System.out.println("Equal to == : "+(a == b));
+        System.out.println("Greater then Equale to >= : " +(a >= b));
+        System.out.println("Lessthen Equale to <= : " +(a <= b));
+        System.out.println("Not Equal to != : "+(a != b));
+    }
+}
