3026de0ca01ae71313ace23fa402eff42709e813
Code With Deep <deeppoptani777@gmail.com>
2025-12-16 12:28:02 +0530

    Create Basics/Operator/Arithmetic.java

---
+++ Java/Chapter 1/Basics/Operator/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic
+{
+    public static void main(String[] args) {
+        int a = 3,b = 2;
+        
+        System.out.println("Addition " +(a + b));
+        System.out.println("Sub " +(a - b));
+        System.out.println("Mul "+(a * b));
+        System.out.println("Div " +(a / b));
+        System.out.println("Mod " +(a % b));
+    }
+}
