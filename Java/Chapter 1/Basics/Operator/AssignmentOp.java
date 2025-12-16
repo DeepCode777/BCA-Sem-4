72ddbae879c63a330672c43c106fd5aeedb4fdde
Code With Deep <deeppoptani777@gmail.com>
2025-12-16 12:50:41 +0530

    Create AssignmentOp.java

---
+++ Java/Chapter 1/Basics/Operator/AssignmentOp.java
@@ -0,0 +1,13 @@
+public class AssignmentOp
+{
+    public static void main(String[] args) {
+        int a = 10;
+        
+        System.out.println(a);
+        a += 10;
+        System.out.println(a);
+        a -= 10;
+        System.out.println(a);
+        
+    }
+}
