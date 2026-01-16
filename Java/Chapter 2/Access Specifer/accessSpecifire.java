088d57877eff32f27397da9d91c03c9e187bee57
Code With Deep <deeppoptani777@gmail.com>
2026-01-16 13:32:53 +0530

    Create Chapter 2/Access Specifer/accessSpecifire.java

---
+++ Java/Chapter 2/Access Specifer/accessSpecifire.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class emp{
+    Scanner sc = new Scanner(System.in);
+    
+    protected int id = sc.nextInt();
+    protected String name = sc.next();
+    protected String add = sc.next();
+}
+
+class dept extends emp{
+    int did = sc.nextInt();
+    String dnm = sc.next();
+    dept (){
+        System.out.println("Employee ID : " +id);
+        System.out.println("Employee Name : " +name);
+        System.out.println("Employee Address : " +add);
+        
+        System.out.println("Department ID : " +did);
+        System.out.println("Depatrment Name : " +dnm);
+    }
+}
+
+class access{
+    public int data = 40; 
+}
+
+class Private{
+    private int num = 30;
+}
+
+public class accessSpecifire
+{
+    public static void main(String[] args) {
+        access r = new access();
+        System.out.println("Data " +r.data);
+        
+        dept d = new dept(); 
+        
+        /*
+        Private p = new Private()
+        System.out.println("Private Data : " + p.num);      // <---<< (Error) 
+        */
+    }
+}
