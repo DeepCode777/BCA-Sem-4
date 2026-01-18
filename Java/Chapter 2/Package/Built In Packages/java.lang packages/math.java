92a87add2655c69e92f1222736d22a231310b2b9
Code With Deep <deeppoptani777@gmail.com>
2026-01-18 12:44:57 +0530

    Create Package/Built In Packages/java.lang packages/math.java

---
+++ Java/Chapter 2/Package/Built In Packages/java.lang packages/math.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class math{
+    public static void main(String[] args) {
+        System.out.println(Math.PI);     //3.14159265...
+        System.out.println(Math.abs(-100));    // 100
+        System.out.println(Math.ceil(10.45));    // 11
+        System.out.println(Math.floor(10.45));    // 10
+        System.out.println(Math.max(10,20));    // 20
+        System.out.println(Math.min(10,20));    // 10
+        System.out.println(Math.sqrt(25));    // 5
+        System.out.println(Math.pow(2,2));    // 4
+    }
+}
+/*
+import java.lang.Math.*;
+System.out.println(sqr(25));     // 5
+*/
