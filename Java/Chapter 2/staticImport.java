7cc39158150696d5d6e5b988f7a63648357eaba3
Code With Deep <deeppoptani777@gmail.com>
2026-01-18 12:32:51 +0530

    Create staticImport.java

---
+++ Java/Chapter 2/staticImport.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*; // Static Import
+
+public class staticImport{
+    public static void main(String[] args) {
+        System.out.println(sqrt(25)); //method
+        System.out.println(pow(2,2));
+        //  package Qualification and direct call method
+    }
+}
