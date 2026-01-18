b09a2dc2ef591bb46758a0f2d1677ef4eca90f25
Code With Deep <deeppoptani777@gmail.com>
2026-01-18 12:29:18 +0530

    Create NormalImport.java

---
+++ Java/Chapter 2/NormalImport.java
@@ -0,0 +1,7 @@
+public class NormalImport{
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(25)); //packag_calss.method
+        System.out.println(Math.pow(2,2));
+        // Access Classes of package Without package Qualification
+    }
+}
