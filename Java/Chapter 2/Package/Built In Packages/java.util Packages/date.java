fad24e3865408b84da082eca0c0579585091bcf2
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 15:58:48 +0530

    Create date.java

---
+++ Java/Chapter 2/Package/Built In Packages/java.util Packages/date.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class date{
+    public static void main(String[] args){
+        Date d = new Date();
+        Date d1 = new Date(26-7-2005); 
+        Date r = new Date(25-4-2005);
+        
+        System.out.println("Current date : " +d);
+        System.out.println("Checking : " +d1.before(d)); 
+        System.out.println("after : " +d1.after(r)); 
+        System.out.println("Clone object : " +d.clone()); 
+        System.out.println("Compare : " +d.compareTo(r)); 
+        System.out.println("Equlas : " +d1.equals(r)); 
+        System.out.println("Time : " +d.getTime()); 
+        System.out.println("Hashcode : " +d.hashCode());
+    }
+}
