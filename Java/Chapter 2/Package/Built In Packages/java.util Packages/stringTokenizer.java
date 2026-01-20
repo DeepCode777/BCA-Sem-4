3b092d0ce0717d599975086c5203858ea4b043fa
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 16:02:40 +0530

    Create stringTokenizer.java

---
+++ Java/Chapter 2/Package/Built In Packages/java.util Packages/stringTokenizer.java
@@ -0,0 +1,11 @@
+import java.util.StringTokenizer;
+
+public class stringTokenizer{
+    public static void main(String[] args){
+        StringTokenizer st = new StringTokenizer("my name is Deep"); 
+        System.out.println("Total number of Tokens :  " +st.countTokens()); 
+        while(st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
+    }
+}
