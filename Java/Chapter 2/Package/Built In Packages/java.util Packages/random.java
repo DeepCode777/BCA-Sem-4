ee0b9f13c6097f299d4bea04bc6e6ca7e3a052cc
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 15:51:12 +0530

    Update random.java

--- Java/Chapter 2/Package/Built In Packages/java.util Packages/random.java
+++ Java/Chapter 2/Package/Built In Packages/java.util Packages/random.java
@@ -2,8 +2,14 @@ import java.util.Random;
 
 public class random{
     public static void main(String[] args){
-        Random r = new Random(); 
+        Random r=new Random(); 
+        System.out.println("Doubles : " +r.doubles());
+        System.out.println("Ints : " +r.ints());
+        System.out.println("Longs : " +r.longs());
+        System.out.println("Next Boolean : " +r.nextBoolean());
         System.out.println("Integer val : " +r.nextInt()); 
         System.out.println("Next decimal : " +r.nextDouble());
+        System.out.println("Next Float : " +r.nextFloat());
+        System.out.println("Next Long : " +r.nextLong());
     }
 }
