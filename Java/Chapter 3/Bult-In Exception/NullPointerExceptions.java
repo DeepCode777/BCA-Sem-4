3866f41e6636b3489eeb908ee6ae7f1b8a58e51d
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 18:02:13 +0530

    Create NullPointerExceptions.java

---
+++ Java/Chapter 3/Bult-In Exception/NullPointerExceptions.java
@@ -0,0 +1,12 @@
+public class NullPointerExceptions
+{
+    public static void main(String[] args) {
+        try{
+            String a = null;
+            System.out.println(a.charAt(0));
+        }
+        catch(NullPointerException e){
+            System.out.println("Null Pointer Exception");
+        }
+    }
+}
