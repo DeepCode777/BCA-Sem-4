92d64e1f1970dc6561713d710b06bd9d75dd0248
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 16:07:28 +0530

    Create calenders.java

---
+++ Java/Chapter 2/Package/Built In Packages/java.util Packages/calenders.java
@@ -0,0 +1,12 @@
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+
+public class calenders{
+    public static void main(String[] args){
+        Calendar cal = Calendar.getInstance(); 
+        GregorianCalendar c = new GregorianCalendar(); 
+        
+        System.out.println("Calendar date : " +cal.getTime()); 
+        System.out.println("Gregorian Calender : " +c.getTime());
+    }
+}
