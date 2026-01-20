d3145bf58a9a4f871c61603ab2eccea1349a29ac
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 15:41:10 +0530

    Create java.util Packages/random.java

---
+++ Java/Chapter 2/Package/Built In Packages/java.util Packages/random.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class random{
+    public static void main(String[] args){
+        Random r = new Random(); 
+        System.out.println("Integer val : " +r.nextInt()); 
+        System.out.println("Next decimal : " +r.nextDouble());
+    }
+}
