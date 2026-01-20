783a658ac47bd29d84de6951c0b6f3de457ce2dc
Code With Deep <deeppoptani777@gmail.com>
2026-01-20 17:19:15 +0530

    Create Vararg.java

---
+++ Java/Chapter 1/OOP/Vararg.java
@@ -0,0 +1,13 @@
+public class Vararg{
+    static void display(int...values){
+        System.out.println("Hello");
+        for(int i : values){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+        display();
+        display(1,2,3,4);
+        display(12,22);
+    }
+}
