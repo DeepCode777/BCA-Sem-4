6226264efb912f9bebf155acc8fdd4f19bfde592
Code With Deep <deeppoptani777@gmail.com>
2026-01-21 09:02:57 +0530

    Create C#/Example.cs

---
+++ C#/Example.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("First Program Of C# Language");
+        }
+    }
+}
